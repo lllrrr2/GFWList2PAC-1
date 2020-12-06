a713839be7185b237ba7056f71496bbc0fe1823b
Zhijie He <hezhijie0327@hotmail.com>
2020-12-06 15:02:43 +0800

    release.sh | Version 1.1.4
    1. <Change> Add a new format called "Shadowrocket".

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.1.3
+# Current Version: 1.1.4
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2PAC.git" && bash ./GFWList2PAC/release.sh
@@ -64,6 +64,21 @@ function GenerateInformation() {
         echo "# Homepage: ${gfwlist2pac_homepage}" >> ../gfwlist2pac_clash_cnacc.yaml
         echo "# Usage: cnVsZS1wcm92aWRlcnM6CiAgZ2Z3bGlzdDJwYWNfY25hY2M6CiAgICBiZWhhdmlvcjogImNsYXNzaWNhbCIKICAgIGludGVydmFsOiAzNjAwCiAgICBwYXRoOiAuL2dmd2xpc3QycGFjX2NuYWNjLnlhbWwKICAgIHR5cGU6IGh0dHAKICAgIHVybDogImh0dHBzOi8vc291cmNlLnpoaWppZS5vbmxpbmUvR0ZXTGlzdDJQQUMvbWFpbi9nZndsaXN0MnBhY19jbGFzaF9jbmFjYy55YW1sIgo=" >> ../gfwlist2pac_clash_cnacc.yaml
     }
+    function gfwlist2pac_shadowrocket() {
+        echo "# Checksum: ${gfwlist2pac_checksum}" > ../gfwlist2pac_shadowrocket.conf
+        echo "# Title: ${gfwlist2pac_title} for Shadowrocket" >> ../gfwlist2pac_shadowrocket.conf
+        echo "# Version: ${gfwlist2pac_version}" >> ../gfwlist2pac_shadowrocket.conf
+        echo "# TimeUpdated: ${gfwlist2pac_timeupdated}" >> ../gfwlist2pac_shadowrocket.conf
+        echo "# Expires: ${gfwlist2pac_expires}" >> ../gfwlist2pac_shadowrocket.conf
+        echo "# Homepage: ${gfwlist2pac_homepage}" >> ../gfwlist2pac_shadowrocket.conf
+        echo "[General]" >> ../gfwlist2pac_shadowrocket.conf
+        echo "bypass-system = true" >> ../gfwlist2pac_shadowrocket.conf
+        echo "bypass-tun = 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 240.0.0.0/4, 255.255.255.255/32" >> ../gfwlist2pac_shadowrocket.conf
+        echo "dns-server = https://dns.alidns.com/dns-query, https://dns.pub/dns-query" >> ../gfwlist2pac_shadowrocket.conf
+        echo "ipv6 = true" >> ../gfwlist2pac_shadowrocket.conf
+        echo "skip-proxy = 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16, localhost, *.local" >> ../gfwlist2pac_shadowrocket.conf
+        echo "[Rule]" >> ../gfwlist2pac_shadowrocket.conf
+    }
     function gfwlist2pac_surge() {
         echo "# Checksum: ${gfwlist2pac_checksum}" > ../gfwlist2pac_surge.yaml
         echo "# Title: ${gfwlist2pac_title} for Surge" >> ../gfwlist2pac_surge.yaml
@@ -91,9 +106,14 @@ function GenerateInformation() {
     gfwlist2pac_autoproxy
     gfwlist2pac_clash
     gfwlist2pac_clash_cnacc
+    gfwlist2pac_shadowrocket
     gfwlist2pac_surge
     gfwlist2pac_surge_cnacc
     gfwlist2pac_quantumult
+}
+# Generate Footer Information
+function GenerateFooterInformation() {
+    echo "FINAL,DIRECT" >> ../gfwlist2pac_shadowrocket.conf
 }
 # Output Data
 function OutputData() {
@@ -101,15 +121,18 @@ function OutputData() {
     for cnacc_data_task in "${!cnacc_data[@]}"; do
         echo "@@||${cnacc_data[cnacc_data_task]}^" >> ../gfwlist2pac_autoproxy.txt
         echo "  - DOMAIN-SUFFIX,${cnacc_data[cnacc_data_task]}" >> ../gfwlist2pac_clash_cnacc.yaml
+        echo "DOMAIN-SUFFIX,${cnacc_data[cnacc_data_task]},DIRECT" >> ../gfwlist2pac_shadowrocket.yaml
         echo "DOMAIN-SUFFIX,${cnacc_data[cnacc_data_task]}" >> ../gfwlist2pac_surge_cnacc.yaml
         echo "DOMAIN-SUFFIX,${cnacc_data[cnacc_data_task]},DIRECT" >> ../gfwlist2pac_quantumult.yaml
     done
     for gfwlist_data_task in "${!gfwlist_data[@]}"; do
         echo "||${gfwlist_data[gfwlist_data_task]}^" >> ../gfwlist2pac_autoproxy.txt
         echo "  - DOMAIN-SUFFIX,${gfwlist_data[gfwlist_data_task]}" >> ../gfwlist2pac_clash.yaml
+        echo "DOMAIN-SUFFIX,${gfwlist_data[gfwlist_data_task]},Proxy" >> ../gfwlist2pac_shadowrocket.yaml
         echo "DOMAIN-SUFFIX,${gfwlist_data[gfwlist_data_task]}" >> ../gfwlist2pac_surge.yaml
         echo "DOMAIN-SUFFIX,${gfwlist_data[gfwlist_data_task]},PROXY" >> ../gfwlist2pac_quantumult.yaml
     done
+    GenerateFooterInformation
     cd .. && rm -rf ./Temp
     exit 0
 }
