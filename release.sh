f5f1e74e141b50c25c457a9561c5d489c0034ed5
Zhijie He <hezhijie0327@hotmail.com>
2020-12-23 13:56:33 +0800

    release.sh | Version 1.2.6
    1. <Change> Fix URL error.
    2. <Change> Minor code.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.2.5
+# Current Version: 1.2.6
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/GFWList2PAC.git" && bash ./GFWList2PAC/release.sh
@@ -9,10 +9,10 @@
 # Get Data
 function GetData() {
     cnacc_domain=(
-        "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2agh_cnacc.txt"
+        "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2agh_blacklist.txt"
     )
     gfwlist_domain=(
-        "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2agh_gfwlist.txt"
+        "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2agh_whitelist.txt"
     )
     rm -rf ./gfwlist2pac_* ./Temp && mkdir ./Temp && cd ./Temp
     for cnacc_domain_task in "${!cnacc_domain[@]}"; do
@@ -24,8 +24,8 @@ function GetData() {
 }
 # Analyse Data
 function AnalyseData() {
-    cnacc_data=($(cat ./cnacc_domain.tmp | grep "\[\|\]" | sed "s/https\:\/\/dns\.alidns\.com\:443\/dns\-query//g;s/https\:\/\/dns\.pub\:443\/dns\-query//g;s/https\:\/\/doh\.opendns\.com\:443\/dns\-query//g;s/tls\:\/\/dns\.alidns\.com\:853//g;s/tls\:\/\/dns\.google\:853//g;s/tls\:\/\/dns\.pub\:853//g;s/\[\///g;s/\/\]//g;s/\//\n/g" | sort | uniq | awk "{ print $2 }"))
-    gfwlist_data=($(cat ./gfwlist_domain.tmp | grep "\[\|\]" | sed "s/https\:\/\/dns\.alidns\.com\:443\/dns\-query//g;s/https\:\/\/dns\.pub\:443\/dns\-query//g;s/https\:\/\/doh\.opendns\.com\:443\/dns\-query//g;s/tls\:\/\/dns\.alidns\.com\:853//g;s/tls\:\/\/dns\.google\:853//g;s/tls\:\/\/dns\.pub\:853//g;s/\[\///g;s/\/\]//g;s/\//\n/g" | sort | uniq | awk "{ print $2 }"))
+    cnacc_data=($(cat ./cnacc_domain.tmp | grep "\/\]https\:\/\/doh\.pub\:443\/dns\-query\|\/\]tls\:\/\/dns\.alidns\.com\:853" | sed "s/https\:\/\/doh\.pub\:443\/dns\-query//g;s/tls\:\/\/dns\.alidns\.com\:853//g;s/\[\///g;s/\/\]//g;s/\//\n/g" | sort | uniq | awk "{ print $2 }"))
+    gfwlist_data=($(cat ./gfwlist_domain.tmp | grep "\/\]https\:\/\/doh\.opendns\.com\:443\/dns\-query\|\/\]tls\:\/\/dns\.google\:853" | sed "s/https\:\/\/doh\.opendns\.com\:443\/dns\-query//g;s/tls\:\/\/dns\.google\:853//g;s/\[\///g;s/\/\]//g;s/\//\n/g" | sort | uniq | awk "{ print $2 }"))
 }
 # Generate Header Information
 function GenerateHeaderInformation() {
